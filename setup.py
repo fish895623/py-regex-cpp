7140c0a8091c30bd24f3758650df4a631daa00da
fish895623 <dan990429+github@gmail.com>
2021-09-23 12:15:46 +0000

    Syntax error

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(
-    name="ignore-parser",
+    name="IgnoreParser",
     version="0.0.1-SNAPSHOT",
     author="fish895623",
     author_email="dan990429+github@gmail.com",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=["ignore-parser"],
+    packages=["IgnoreParser"],
 )
